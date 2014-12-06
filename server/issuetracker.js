3772ea6160ed9aa87141edeca415fc3539ee120c
Brandon <turk.brandon@gmail.com>
2014-12-06 13:31:42 -0800

    Recaptcha added to Suggestion Box

--- server/issuetracker.js
+++ server/issuetracker.js
@@ -17,18 +17,32 @@ if (config.SUGGESTIONBOX){
     try {
       var form = new formidable.IncomingForm();
       form.parse(req, function(err, fields) {
+
+        var labels = [];
+        if (fields.bug)
+          labels.push(fields.bug);
+        if (fields.enhancement)
+          labels.push(fields.enhancement);
+        if (fields.question)
+          labels.push(fields.question);
+        if (!fields['g-recaptcha-response'].length){
+          resp.end('Invalid Captcha Response. Please return to the previous page.');
+          return;
+        }
         github.issues.create({
           user: 'USERNAMEHERE',
           repo: 'REPONAMEHERE',
           title: fields.title,
           body: fields.body,
-          labels: ( fields.labels ? fields.labels.split(' ') : [] )
+          labels: labels
         });
+        if (fields['g-recaptcha-response'].length)
+          resp.end('Submitted~ Please close this window.');
       });
     }
     catch (e) {
       winston.error('formidable threw ' + e);
+      resp.end('Unknown error, please inform host.');
     }
-    resp.end('Submitted~ Please close this window.');
   }
 }
