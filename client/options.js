e9d46ecf0d3a188d187dd62164f35d279ca37bfc
bakape <scorpid33@gmail.com>
2014-12-21 01:56:02 +0200

    Optional dynamic post unloading
    Resolves bakape#105
    
    Conflicts:
    	client/options.js

--- client/options.js
+++ client/options.js
@@ -53,6 +53,7 @@ optSpecs.push(option_theme);
 optSpecs.push(option_user_bg);
 optSpecs.push(option_user_bg_image);
 optSpecs.push(option_last_n);
+optSpecs.push(option_postUnloading);
 optSpecs.push(option_clean_ls);
 
 nashi.upload = !!$('<input type="file"/>').prop('disabled');
@@ -121,6 +122,17 @@ oneeSama.lastN = options.get('lastn');
 options.on('change:lastn', function (model, lastN) {
 	oneeSama.lastN = lastN;
 });
+
+/* KEEP THREAD LENGTH WITHIN LASTN */
+
+function option_postUnloading(toggle){}
+
+option_postUnloading.id = 'postUnloading';
+option_postUnloading.label = 'Dynamic Post Unloading';
+option_postUnloading.type = 'revcheckbox';
+option_postUnloading.tooltip = 'Improves thread responsiveness by unloading posts from the'+
+		' top of the thread, so that post count stays within the Last # value. Only applies to '+
+		'Last # enabled threads';
 
 /* THEMES */
 
