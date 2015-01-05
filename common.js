d92ade6cdcb2c0e5005d512c314c186610b72092
bakape <scorpid33@gmail.com>
2015-01-05 15:32:09 +0200

    Refactor OS.gazou_img()
    Conflicts:
    	common.js

--- common.js
+++ common.js
@@ -614,20 +614,13 @@ OS.gazou_img = function (info, toppu) {
 		src = encodeURI('../outbound/hash/' + info.MD5);
 		thumb = imgPaths.vint + info.vint;
 	}
+	else if (m && this.autoGif)
+		thumb = src;
 	else if (this.thumbStyle != 'small' && info.mid) {
 		thumb = encodeURI(imgPaths.mid + info.mid);
 		if (!toppu && this.thumbStyle == 'large') {
 			tw *= 2;
 			th *= 2;
-		}
-	}
-	else if (this.spoilToggle && info.realthumb) {
-		thumb = encodeURI(imgPaths.thumb + info.realthumb);
-		if (w > h) {
-			th = Math.round(tw/w*h);
-		}
-		else {
-			tw = Math.round(th/h*w);
 		}
 	}
 	else if (info.thumb)
