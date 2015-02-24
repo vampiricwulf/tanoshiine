61140c1b3b55d6993fec385b8429253b48ad3eaf
bakape <scorpid33@gmail.com>
2015-02-24 03:29:46 +0100

    Custom URLs in board navigation

--- server/state.js
+++ server/state.js
@@ -212,6 +212,7 @@ function make_navigation_html() {
 	if (!HOT.INTER_BOARD_NAVIGATION)
 		return '';
 	var bits = ['<b id="navTop">['];
+	// Actual boards
 	config.BOARDS.forEach(function (board, i) {
 		if (board == config.STAFF_BOARD)
 			return;
@@ -219,6 +220,10 @@ function make_navigation_html() {
 			bits.push(' / ');
 		bits.push('<a href="../'+board+'/">'+board+'</a>');
 	});
+	// Add custom URLs to board navigation
+	config.PSUEDO_BOARDS.forEach(function(item) {
+		bits.push(' / <a href="'+item[1]+'/">'+item[0]+'</a>')
+	})
 	bits.push(']</b>');
 	return bits.join('');
 }
