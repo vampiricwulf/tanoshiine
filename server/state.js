13fde664be96b58e25f2585a72eee55e94bbb96b
bakape <scorpid33@gmail.com>
2015-03-01 03:59:19 +0100

    Reformat server/state.js

--- server/state.js
+++ server/state.js
@@ -55,15 +55,33 @@ function reload_hot_config(cb) {
 
 		// Pass some of the config variables to the client
 		// TODO: Once using gulp, could write this to the start of the client file instead
-		var clientHot = _.pick(HOT, 'CUSTOM_BANNER_BOTTOM', 'ILLYA_DANCE', 'EIGHT_BALL', 'THREADS_PER_PAGE', 'THREAD_LAST_N',
-      'ABBREVIATED_REPLIES', 'SUBJECT_MAX_LENGTH', 'EXCLUDE_REGEXP','ADMIN_ALIAS', 'MOD_ALIAS', 'SAGE_ENABLED');
+		var clientHot = _.pick(HOT,
+			'CUSTOM_BANNER_BOTTOM',
+			'ILLYA_DANCE',
+			'EIGHT_BALL',
+			'THREADS_PER_PAGE',
+			'ABBREVIATED_REPLIES',
+			'SUBJECT_MAX_LENGTH',
+			'EXCLUDE_REGEXP',
+			'ADMIN_ALIAS',
+			'MOD_ALIAS',
+			'SAGE_ENABLED',
+			'THREAD_LAST_N'
+		);
 		HOT.CLIENT_CONFIG = JSON.stringify(clientConfig);
 		HOT.CLIENT_IMAGER = JSON.stringify(clientImager);
 		HOT.CLIENT_REPORT = JSON.stringify(clientReport);
 		HOT.CLIENT_HOT = JSON.stringify(clientHot);
-		var combined = exports.clientConfig = [clientConfig, clientImager, clientReport, clientHot];
-		exports.clientConfigHash = HOT.CLIENT_CONFIG_HASH = crypto.createHash('MD5')
-				.update(JSON.stringify(combined)).digest('hex');
+		var combined = exports.clientConfig = [
+			clientConfig,
+			clientImager,
+			clientReport,
+			clientHot
+		];
+		exports.clientConfigHash = HOT.CLIENT_CONFIG_HASH = crypto
+			.createHash('MD5')
+			.update(JSON.stringify(combined))
+			.digest('hex');
 
 		read_exits('exits.txt', function () {
 			hooks.trigger('reloadHot', HOT, cb);
@@ -71,10 +89,25 @@ function reload_hot_config(cb) {
 	});
 }
 
-var clientConfig = _.pick(config,'IP_MNEMONIC', 'GAME_BOARDS', 'USE_WEBSOCKETS', 'SOCKET_PATH', 'DEBUG',
-	'READ_ONLY', 'API_URL', 'IP_TAGGING');
-var clientImager = _.pick(imager,'WEBM', 'AUDIOFILES', 'UPLOAD_URL','MEDIA_URL', 'THUMB_DIMENSIONS',
-	'PINKY_DIMENSIONS', 'SPOILER_IMAGES', 'IMAGE_HATS');
+var clientConfig = _.pick(config,
+	'IP_MNEMONIC',
+	'USE_WEBSOCKETS',
+	'SOCKET_PATH',
+	'DEBUG',
+	'READ_ONLY',
+	'API_URL',
+	'IP_TAGGING',
+	'RADIO'
+);
+var clientImager = _.pick(imager,
+	'WEBM',
+	'UPLOAD_URL',
+	'MEDIA_URL',
+	'THUMB_DIMENSIONS',
+	'PINKY_DIMENSIONS',
+	'SPOILER_IMAGES',
+	'IMAGE_HATS'
+);
 var clientReport = _.pick(report, 'RECAPTCHA_PUBLIC_KEY');
 
 function reload_scripts(cb) {
@@ -151,7 +184,8 @@ function expand_templates(res) {
 	templateVars.FAQ = build_FAQ(templateVars.FAQ);
 	// Format info banner
 	if (templateVars.BANNERINFO)
-		templateVars.BANNERINFO = '&nbsp;&nbsp;&nbsp;[' + templateVars.BANNERINFO + ']';
+		templateVars.BANNERINFO = '&nbsp;&nbsp;&nbsp;['
+				+ templateVars.BANNERINFO + ']';
 
 	function tmpl(data) {
 		var expanded = _.template(data)(templateVars);
@@ -191,11 +225,13 @@ function build_FAQ(faq){
 }
 
 function buildClient(cb){
-	exec('./node_modules/gulp/bin/gulp.js client mod vendor', function(err, stdout, stderr){
-		if (err)
-			return console.error('Error: Failed to build client:', err, stderr);
-		cb();
-	});
+	exec('./node_modules/gulp/bin/gulp.js client mod vendor',
+		function(err, stdout, stderr){
+			if (err)
+				return console.error('Error: Failed to build client:', err, stderr);
+			cb();
+		}
+	);
 }
 
 exports.reload_hot_resources = function (cb) {
@@ -222,8 +258,8 @@ function make_navigation_html() {
 	});
 	// Add custom URLs to board navigation
 	config.PSUEDO_BOARDS.forEach(function(item) {
-		bits.push(' / <a href="'+item[1]+'/">'+item[0]+'</a>')
-	})
+		bits.push(' / <a href="'+item[1]+'/">'+item[0]+'</a>');
+	});
 	bits.push(']</b>');
 	return bits.join('');
 }
@@ -232,7 +268,7 @@ function read_exits(file, cb) {
 	fs.readFile(file, 'UTF-8', function (err, lines) {
 		if (err)
 			return cb(err);
-		var exits = [], dest = HOT.BANS;
+		var dest = HOT.BANS;
 		lines.split(/\n/g).forEach(function (line) {
 			var m = line.match(/^(?:^#\d)*(\d+\.\d+\.\d+\.\d+)/);
 			if (!m)
